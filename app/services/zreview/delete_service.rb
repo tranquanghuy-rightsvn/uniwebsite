6e68ed4535b54412abc2a175ee9b6f7e2563d3b2
Quang Huy <tranquanghuyblackberry@gmail.com>
2024-03-21 20:11:37 +0700

    Add update for zreview

--- app/services/zreview/delete_service.rb
+++ app/services/zreview/delete_service.rb
@@ -1,4 +1,4 @@
-class Product::DeleteService
+class Zreview::DeleteService
   attr_accessor :resource
 
   def initialize resource
@@ -12,43 +12,7 @@ class Product::DeleteService
       File.delete(file_path)
     end
 
-    # Lấy trang news và sửa
-    root_news_path = Rails.root.join('projects', 'chaos_news', 'tin-tức-như-quỳnh-vinpearl.html')
-    doc_list_news = Nokogiri::HTML(File.read(root_news_path))
-    list_news = doc_list_news.at_css('#list-news')
-
-    list_news_html = ""
-
-    Product::ChaosNews.order(id: :desc).limit(30).map do |product|
-      product_img= product.image.url.split("/")[-3..-1].join("/")
-      list_news_html += "<div class='news-item col-md-6 col-sm-12'>
-                                  <div class='row ht_reset_margin'>
-                                    <div class='img-block col-md-12 col-sm-6 ht_reset_pd'>
-                                      <div class='img-wrapper img-fit'>
-                                        <a href='./" + product.url + "'>
-                                          <img src='" + product_img + "' alt='" + product.title + "' width='364' height='302' class='ht_mobile_hide'>
-                                          <img src='" + product_img + "' alt='" + product.title + "' width='164' height='105' class='ht_mobile_show'>
-                                        </a>
-                                      </div>
-                                    </div>
-                                    <div class='info-block col-md-12 col-sm-6 ht_reset_pd'>
-                                      <div class='info-wrapper'>
-                                        <a href='./" + product.url + "'>
-                                          <h3 class='news-item-tit three_dots_2'>" + product.title + "</h3>
-                                        </a>
-                                        <div class='data_n_view'>
-                                          <span class='date'>" + product.time_publish.to_s + "</span>
-                                        </div>
-                                        <div class='summary three_dots_2' title='" + product.description + "'>" + product.description + "</div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>"
-    end
-    list_news.inner_html = list_news_html
-    File.write(root_news_path, doc_list_news.to_html)
-
-    chaos_news_path = Rails.root.join('projects', 'chaos_news')
+    chaos_news_path = Rails.root.join('projects', 'zreview')
     system('cd ' + chaos_news_path.to_s + ' && git add . && git commit -m "New commit" && git push origin master -f')
   end
 end
--- app/services/zreview/update_service.rb
+++ app/services/zreview/update_service.rb
@@ -1,4 +1,4 @@
-class Product::UpdateService
+class Zreview::UpdateService
   attr_accessor :resource
 
   def initialize resource
@@ -6,120 +6,353 @@ class Product::UpdateService
   end
 
   def perform
-    # Lấy bài viết gốc và sửa
-    content_temp_path = Rails.root.join('projects', 'chaos_news', 'du-lich-tet-ha-noi-2024.txt')
+    category_direct_path = case resource.category.name
+      when "Công nghệ"
+        "cong-nghe.html"
+      when "Sách"
+        "sach.html"
+      when "Phim"
+        "phim.html"
+      when "Khách sạn"
+        "khach-san.html"
+      when "Ẩm thực"
+        "am-thuc.html"
+      when "Nhân vật"
+        "nhan-vat.html"
+      when "Đồ gia dụng"
+        "do-gia-dung.html"
+      when "Nhà hàng"
+        "nha-hang.html"
+      when "Du lịch"
+        "du-lich.html"
+      when "Thực phẩm chức năng"
+        "thuc-pham-chuc-nang.html"
+      when "Khác"
+        "khac.html"
+      end
+
+    # lấy bài viết gốc và sửa
+    content_temp_path = Rails.root.join('projects', 'zreview', 'bai-viet-mau.txt')
 
     content_temp = File.read(content_temp_path)
     resource_img = resource.image.url.split("/")[-3..-1].join("/")
     content_html = "<!DOCTYPE html>
-    <html lang='vi' dir='ltr' prefix='og: https://ogp.me/ns#' class=' widescreen entity.node.canonical'>
+    <html lang='vi' xmlns='http://www.w3.org/1999/xhtml'>
     <head>
-      <script async src='https://www.googletagmanager.com/gtag/js?id=G-H8GG5LDZG0'></script>
+      <script async src='https://www.googletagmanager.com/gtag/js?id=G-00HNHTJ14N'></script>
       <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-H8GG5LDZG0');
+        gtag('config', 'G-00HNHTJ14N');
       </script>
-      <meta name='format-detection' content='telephone=no' />
-      <meta charset='utf-8' />
-      <link rel='canonical' href='./" + resource.url + "' />
-      <meta name='robots' content='index, follow' />
-      <link rel='image_src' href='" + resource_img + "' />
-      <meta name='description' content='" + resource.description + "' />
-      <meta property='og:site_name' content='NhuQuynhVinpearl' />
-      <meta property='og:url' content='./" + resource.url + "' />
-      <meta property='og:title' content='" + resource.title + "' />
-      <meta property='og:description' content='" + resource.description + "' />
-      <meta property='og:image' content='" + resource_img  + "' />
-      <meta property='og:image:url' content='" + resource_img + "' />
-      <meta property='og:image:secure_url' content='" + resource_img + "' />
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+      <meta http-equiv='REFRESH' content='1800' />
 
-      <script type=\"application/ld+json\">
+      <title>" + resource.title_format + "</title>
+      <meta name='description' content='" + resource.description.gsub('"', "") + "' />
+      <meta name='keywords' content='" + JSON.parse(resource.keywords).join(",") + "' />
+
+      <!-- ROBOTS -->
+      <meta name='googlebot' content='noarchive' />
+      <meta name='robots' content='noarchive,index,follow' />
+
+      <!-- FACEBOOK OPEN GRAPH -->
+      <meta property='fb:app_id' content='571421367282732' />
+      <meta property='og:site_name' content='Zreview.vn' />
+      <meta property='og:rich_attachment' content='true' />
+      <meta property='og:type' content='article' />
+
+      <meta property='article:published_time' content='" + resource.formated_time + "' />
+
+      <meta property='og:url' content='https://zreview.vn/" + resource.url + "' />
+      <meta property='og:title' content='" + resource.title.gsub('"', "") + "' />
+      <meta property='og:description' content='" + resource.description.gsub('"', "") + "' />" +
+      JSON.parse(resource.keywords).map do |key|
+        "<meta property='article:tag' content='" + key + "'/>
+        "
+      end.join("") +
+      "<link rel='canonical' href='https://zreview.vn/" + resource.url + "'/>
+
+      <meta content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1' name='viewport'/>
+      <meta name='format-detection' content='telephone=no' />
+      <meta name='format-detection' content='address=no' />
+      <meta name='apple-mobile-web-app-capable' content='yes'>
+      <meta name='apple-mobile-web-app-status-bar-style' content='black'>
+      <meta name='referrer' content='no-referrer-when-downgrade' />
+
+      <script type='application/ld+json'>
         {
           \"@context\": \"http://schema.org\",
-          \"@type\": \"Article\",
-          \"headline\": \"" + resource.title.gsub("\"", "") + "\",
-          \"dateCreated\": \"" + resource.formated_time.to_s + "\",
-          \"datePublished\": \"" + resource.formated_time.to_s + "\",
-          \"dateModified\": \"" + resource.formated_time.to_s + "\",
-          \"author\": {
-            \"@type\": \"Person\",
-            \"name\": \"Như Quỳnh Vinpearl\"
-          },
-          \"publisher\": {
-            \"@type\": \"Organization\",
-            \"name\": \"nhuquynhvinpearl.com\"
-          },
-          \"description\": \"" + resource.description.gsub("\"", "") + "\",
-          \"image\": #{resource.content.embeds.map{|e| e.url }},
-          \"url\": \"https://nhuquynhvinpearl.com/" + resource.url + "\",
-          \"keywords\": #{resource.keywords},
-          \"logo\": {
-            \"@type\": \"ImageObject\",
-            \"url\": \"https://nhuquynhvinpearl.com/" + resource.image.url.split("/")[-3..-1].join("/") + "\"
-          },
+          \"@type\": \"NewsArticle\",
           \"mainEntityOfPage\": {
-            \"@type\": \"WebPage\",
-            \"url\": \"https://nhuquynhvinpearl.com/" + resource.url + "\",
-            \"@id\": \"https://nhuquynhvinpearl.com/" + resource.url + "\"
-          }
-        }
-      </script>
-    <link rel='revision' href='./" + resource.url + "' />
-    <title>" + resource.title + "</title>" + content_temp
+          \"@type\": \"WebPage\",
+          \"@id\": \"https://zreview.vn/" + resource.url + "\"
+        },
+        \"headline\": \"" + resource.title.gsub('"', "") + "\",
+        \"description\": \"" + resource.description.gsub('"', "") + "\",
+        \"image\": {
+        \"@type\": \"ImageObject\",
+        \"url\": \"https://zreview.vn/" + resource_img + "\",
+        \"width\": 500,
+        \"height\": 333
+      },
+      \"datePublished\": \"" + resource.formated_time + "\",
+      \"author\": {
+      \"@type\": \"Organization\",
+      \"name\": \"Zreview\"
+    },
+    \"publisher\": {
+    \"@type\": \"Organization\",
+    \"name\": \"Zreview\"
+    }
+    }
+    var cate_path = " + category_direct_path[0...-5] + ";
+    var cate_name = " + resource.category.name + ";
+    </script>" + content_temp
 
     doc_root_post = Nokogiri::HTML(content_html.to_s)
-    title_root_post = doc_root_post.at_css('#title-news')
-    title_root_post.inner_html = resource.title
-    content_root_post = doc_root_post.at_css('#content-news')
+    title_root_post = doc_root_post.at_css('#title-review')
+    title_root_post.inner_html = resource.title_format
+    content_root_post = doc_root_post.at_css('#content-review')
     content_root_post.inner_html = resource.content.body.to_s.gsub(/<action-text-attachment[^>]*>[^<]*<\/action-text-attachment>/, '')
-    date_root_post = doc_root_post.at_css('#date-news')
+    date_root_post = doc_root_post.at_css('#time-review')
     date_root_post.inner_html = resource.time_publish.to_s
-    description_root_post = doc_root_post.at_css('#description-news')
-    description_root_post.inner_html = resource.description
 
-    doc_new_path = Rails.root.join('projects', 'chaos_news', resource.url)
+    category_root_post = doc_root_post.at_css('#zreview-parent-category')
+    category_root_post.inner_html = "<a href='./" + category_direct_path + "' title='" + resource.category.name + "' class='parent_cate' id='zreview-parent-cate'>" + resource.category.name + "</a>"
+
+    news_reference = doc_root_post.at_css('#news-reference')
+    list_news_html = ""
+    Product::Zreview.same_category(resource).order(id: :desc).limit(6).offset(1).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article zone-ad-name='' class='article-item znews-native type-text picked-featured'>
+         <p class='article-thumbnail'>
+            <a href='" + product.url + "'>
+              <img src='" + product_img + "'  alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+
+            <ul class='article-related'>
+              <li class=' type-text'><a href='" + product.url + "' title='" + product.title_format + "'>" + product.title_format + "</a></li>
+            </ul>
+          </header>
+        </article>"
+    end
+
+    news_reference.inner_html = list_news_html
+
+    doc_new_path = Rails.root.join('projects', 'zreview', resource.url)
     File.write(doc_new_path, doc_root_post.to_html)
 
-    # Lấy trang news và sửa
-    root_news_path = Rails.root.join('projects', 'chaos_news', 'tin-tức-như-quỳnh-vinpearl.html')
+
+    # Lấy trang index và sửa
+
+    root_news_path = Rails.root.join('projects', 'zreview', 'index.html')
     doc_list_news = Nokogiri::HTML(File.read(root_news_path))
-    list_news = doc_list_news.at_css('#list-news')
+    list_news_1 = doc_list_news.at_css('#list-first')
 
     list_news_html = ""
 
-    Product::ChaosNews.order(id: :desc).limit(30).map do |product|
+    Product::Zreview.hot_category.order(id: :desc).limit(3).map do |product|
       product_img= product.image.url.split("/")[-3..-1].join("/")
-      list_news_html += "<div class='news-item col-md-6 col-sm-12'>
-                                  <div class='row ht_reset_margin'>
-                                    <div class='img-block col-md-12 col-sm-6 ht_reset_pd'>
-                                      <div class='img-wrapper img-fit'>
-                                        <a href='./" + product.url + "'>
-                                          <img src='" + product_img + "' alt='" + product.title + "' width='364' height='302' class='ht_mobile_hide'>
-                                          <img src='" + product_img + "' alt='" + product.title + "' width='164' height='105' class='ht_mobile_show'>
-                                        </a>
-                                      </div>
-                                    </div>
-                                    <div class='info-block col-md-12 col-sm-6 ht_reset_pd'>
-                                      <div class='info-wrapper'>
-                                        <a href='./" + product.url + "'>
-                                          <h3 class='news-item-tit three_dots_2'>" + product.title + "</h3>
-                                        </a>
-                                        <div class='data_n_view'>
-                                          <span class='date'>" + product.time_publish.to_s + "</span>
-                                        </div>
-                                        <div class='summary three_dots_2' title='" + product.description + "'>" + product.description + "</div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>"
+      list_news_html += "<article zone-ad-name='' class='article-item znews-native type-text picked-featured'>
+         <p class='article-thumbnail'>
+            <a href='" + product.url + "'>
+              <img src='" + product_img + "'  alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+
+            <ul class='article-related'>
+              <li class=' type-text'><a href='" + product.url + "' title='" + product.title_format + "'>" + product.title_format + "</a></li>
+            </ul>
+          </header>
+        </article>"
     end
-    list_news.inner_html = list_news_html
+    list_news_1.inner_html = list_news_html
+
+    list_news_2 = doc_list_news.at_css('#list-second')
+
+    list_news_html = ""
+
+    Product::Zreview.hot_category.order(id: :desc).limit(5).offset(3).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article class='article-item type-text picked-trending short'>
+          <p class='article-thumbnail'>
+            <a href='" + product.url + "'>
+
+              <img src='" + product_img + "'  alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+
+          </header>
+        </article>"
+    end
+    list_news_2.inner_html = list_news_html
+
+    list_news_3 = doc_list_news.at_css('#list-human')
+    list_news_html = ""
+
+    Product::Zreview.by_humen.order(id: :desc).limit(5).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article class='article-item znews-native type-picture picked-multi short'>
+          <p class='article-thumbnail'>
+            <a href='./" + product.url + "'>
+
+              <img src='" + product_img + "' alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='./" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+          </header>
+        </article>"
+    end
+
+    list_news_3.inner_html = list_news_html
+
+    list_news_4 = doc_list_news.at_css('#list-book')
+    list_news_html = ""
+
+    Product::Zreview.by_books.order(id: :desc).limit(3).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article class='article-item znews-native type-text'>
+          <p class='article-thumbnail'>
+            <a href='./" + product.url + "'>
+
+              <img src='" + product_img + "' alt='" + product.title_format + "' alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='./" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+          </header>
+        </article>"
+    end
+
+    list_news_4.inner_html = list_news_html
+
+    list_news_5 = doc_list_news.at_css('#list-film')
+    list_news_html = ""
+
+    list_news_10 = doc_list_news.at_css('#top-list-book')
+    list_news_html = ""
+
+    Product::Zreview.by_books.order(id: :desc).limit(5).offset(3).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article class='article-item znews-native type-text'>
+          <p class='article-thumbnail'>
+            <a href='./" + product.url + "'>
+
+              <img src='" + product_img + "' alt='" + product.title_format + "' alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='./" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+          </header>
+        </article>"
+    end
+
+    list_news_10.inner_html = list_news_html
+
+    list_news_11 = doc_list_news.at_css('#top-list-film')
+    list_news_html = ""
+
+    Product::Zreview.by_films.order(id: :desc).limit(5).offset(3).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article class='article-item znews-native type-text'>
+          <p class='article-thumbnail'>
+            <a href='./" + product.url + "'>
+
+              <img src='" + product_img + "' alt='" + product.title_format + "' alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='./" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+          </header>
+        </article>"
+    end
+
+    list_news_11.inner_html = list_news_html
+
+    list_news_5 = doc_list_news.at_css('#list-film')
+    list_news_html = ""
+
+    Product::Zreview.by_films.order(id: :desc).limit(3).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article class='article-item znews-native type-text'>
+          <p class='article-thumbnail'>
+            <a href='./" + product.url + "'>
+
+              <img src='" + product_img + "' alt='" + product.title_format + "' alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='./" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+          </header>
+        </article>"
+    end
+
+    list_news_5.inner_html = list_news_html
+
+    list_news_6 = doc_list_news.at_css('#list-recommend')
+    list_news_html = ""
+    hot_product_ids = Product::Zreview.hot_category.order(id: :desc).limit(8).ids
+    Product::Zreview.not_ids(hot_product_ids).order(id: :desc).limit(12).map do |product|
+      product_img= product.image.url.split("/")[-3..-1].join("/")
+      list_news_html += "<article class='article-item  type-text'>
+          <p class='article-thumbnail'>
+            <a href='./" + product.url + "'>
+
+              <img src='" + product_img + "' alt='" + product.title_format + "' alt='" + product.title_format + "' />
+            </a>
+          </p>
+          <header>
+            <p class='article-title'>
+              <a href='./" + product.url + "'>" + product.title_format + "</a>
+            </p>
+            <p class='article-summary'>" + product.description + "</p>
+          </header>
+        </article>"
+    end
+
+    list_news_6.inner_html = list_news_html
+
     File.write(root_news_path, doc_list_news.to_html)
 
-    chaos_news_path = Rails.root.join('projects', 'chaos_news')
-    system('cd ' + chaos_news_path.to_s + ' && git add . && git commit -m "New commit" && git push origin master -f')
+    # sửa category nếu thay đổi
+    # khỏi đi
+
+    zreview_path = Rails.root.join('projects', 'zreview')
+    system('cd ' + zreview_path.to_s + ' && git add . && git commit -m "New commit" && git push origin master -f')
   end
 end
